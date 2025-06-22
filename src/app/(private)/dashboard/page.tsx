7b62cc52661ebfaf5f78323f33d71c75c9bcaac7
lucas <id.lucas@outlook.com.br>
2025-06-22 00:35:57 -0300

    feat: post cards mapping

--- src/app/(private)/dashboard/page.tsx
+++ src/app/(private)/dashboard/page.tsx
@@ -1,16 +1,32 @@
 "use client";
 
 import React from "react";
-import { useAuth } from "@/hooks/useAuth";
+
+import PostCard from "@/components/PostCard";
+import { usePosts } from "@/hooks/usePosts";
+import { formatDate } from "@/utils/format-date";
 
 const Dashboard = () => {
-  const { user } = useAuth();
-
-  console.log(user);
+  const { posts } = usePosts();
 
   return (
-    <div className="flex h-screen flex-col items-center justify-center">
-      <h1 className="text-2xl font-bold">Em construção</h1>
+    <div className="flex min-h-screen flex-col items-center gap-4 p-4">
+      {posts.map((post) => {
+        return (
+          <PostCard
+            key={post.id}
+            id={post.id}
+            authorName={"Lucas Lima"}
+            authorTime={formatDate(post.createdAt)}
+            authorAvatar="https://i.pravatar.cc/"
+            postText={post.content}
+            likes={post.likes}
+            commentsCount={post.comments?.length}
+            userAvatar="https://i.pravatar.cc/"
+            comments={post.comments}
+          />
+        );
+      })}
     </div>
   );
 };
